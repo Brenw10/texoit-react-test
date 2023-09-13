2404d6853e10b00e76053be95043307b8eafd08c
Brendon Mota <bw.bwctm@gmail.com>
2023-09-13 18:05:27 -0300

    types

--- src/movie/useMovies/index.ts
+++ src/movie/useMovies/index.ts
@@ -1,5 +1,6 @@
 import TexoItApi from "@/core/TexoItApi";
 import { useQuery } from "@tanstack/react-query"
+import { IMovie } from "../types";
 
 export interface IMoviesPayload {
   page: number;
@@ -8,8 +9,13 @@ export interface IMoviesPayload {
   winner?: string;
 };
 
+interface IData {
+  content: IMovie[],
+  totalElements: number;
+}
+
 export default function useMovies(payload: IMoviesPayload) {
-  return useQuery({
+  return useQuery<IData>({
     queryKey: ['movies', payload],
     queryFn: async () => {
       const response = await TexoItApi.get('/movies', { params: payload });
