c1d7955319ce139dabc49555202d59d2fd23389b
Brendon Mota <bw.bwctm@gmail.com>
2023-09-13 13:49:14 -0300

    active

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -2,14 +2,17 @@
 
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import styles from './style.module.css';
+import { usePathname } from 'next/navigation'
 
 export default function SideBar() {
+  const pathname = usePathname();
+
   return (
     <Navbar expand="lg" className={`bg-body-tertiary h-100 align-items-start ${styles.navbar}`} data-bs-theme="light">
       <Container>
-        <Nav className="me-auto flex-column">
-          <Nav.Link href="/">Dashboard</Nav.Link>
-          <Nav.Link href="/movies">List</Nav.Link>
+        <Nav className="flex-column" activeKey={pathname}>
+          <Nav.Link eventKey='/' href="/">Dashboard</Nav.Link>
+          <Nav.Link eventKey='/movies' href="/movies">List</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
